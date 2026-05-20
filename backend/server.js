df3adf6751131d281ef543d737256483800e43c5
Rohan6439 <rohankhanna6439@gmail.com>
2026-05-20 21:18:23 +0530

    bakend api change for deployement

--- backend/server.js
+++ backend/server.js
@@ -66,6 +66,8 @@ const cache = {};
 
 const CACHE_TIME = 5 * 60 * 1000;
 
+const PORT = process.env.PORT || 5000;
+
 async function getStockData(symbol) {
   try {
     const now = Date.now();
@@ -221,6 +223,6 @@ app.get("/", (req, res) => {
   res.send("Portfolio API Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
